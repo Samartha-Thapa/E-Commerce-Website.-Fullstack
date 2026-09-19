f34025e08365eafbd93351fd84e677977912db34
Sam Magar <magar.samartha2@gmail.com>
2026-09-19 17:31:33 +0545

    feat(database) added databse tables for categories, products, addresses and updated users table

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role')->default('user')->nullable();
+            $table->string('role')->default('user');
             $table->string('verification_code')->nullable();
             $table->boolean('is_verified')->default(false);
             $table->timestamp('verification_expires_at')->nullable();
